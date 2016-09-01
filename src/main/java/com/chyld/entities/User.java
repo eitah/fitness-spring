c6e6b171d7336570232247fd86a5f4b7ab29a6db
localadmin <araam@allstate.com>
2016-09-01 11:46:28 -0500

    set up one to many relationship for user & exercises

--- src/main/java/com/chyld/entities/User.java
+++ src/main/java/com/chyld/entities/User.java
@@ -26,6 +26,7 @@ public class User implements UserDetails {
     private Profile profile;
     private Date created;
     private Date modified;
+    private List<Exercise> exercises;
 
     @Id
     @GeneratedValue
@@ -51,6 +52,15 @@ public class User implements UserDetails {
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     public List<Role> getRoles() {return roles;}
     public void setRoles(List<Role> roles) {this.roles = roles;}
+
+
+    @OneToMany(cascade = CascadeType.DETACH, mappedBy = "user")
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
 
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
     public Profile getProfile() {return profile;}
--- src/main/resources/db/migration/V9__populate_data.sql
+++ src/main/resources/db/migration/V9__populate_data.sql
@@ -8,3 +8,13 @@ INSERT INTO `fitness`.`roles_users` (`role_id`, `user_id`) VALUES ('2', '1');
 
 INSERT INTO `fitness`.`profiles` (`version`, `gender`, `age`, `height`, `weight`, `photo`, `user_id`)
 VALUES ('0', 'M', '25', '70', '165', 'http://findicons.com/files/icons/1072/face_avatars/300/i05.png', '1');
+
+INSERT INTO `fitness`.`exercises`
+(`version`, `type`, `quantity`, `calories`, `duration`, `user_id`)
+VALUES ('0', 'BIKE', '5', '300', '30', '1');
+INSERT INTO `fitness`.`exercises`
+(`version`, `type`, `quantity`, `calories`, `duration`, `user_id`)
+VALUES ('0', 'SWIM', '5', '100', '100', '1');
+INSERT INTO `fitness`.`exercises`
+(`version`, `type`, `quantity`, `calories`, `duration`, `user_id`)
+VALUES ('0', 'RUN', '5', '200', '30', '1');
